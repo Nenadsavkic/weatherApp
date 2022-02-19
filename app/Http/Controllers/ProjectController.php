695d23bcd4d871dbd00adefb6b4e279025d7c288
Nenad Savkic <savkicn@gmail.com>
2022-02-19 20:36:57 +0100

    finished

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,18 +9,19 @@ class ProjectController extends Controller
 {
     public function welcome(Request $request)
     {
+        $myToken = "82e0be784317765ec3af35fc2e2d4aef";
         $city1 = "Belgrade";
         $city2 = "New York";
         $city3 = "London";
         $city4 = "Rome";
         $city5 = "Moscow";
         $city6 = "Paris";
-        $response1 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city1."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
-        $response2 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city2."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
-        $response3 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city3."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
-        $response4 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city4."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
-        $response5 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city5."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
-        $response6 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city6."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
+        $response1 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city1."&units=metric&appid=".$myToken);
+        $response2 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city2."&units=metric&appid=".$myToken);
+        $response3 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city3."&units=metric&appid=".$myToken);
+        $response4 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city4."&units=metric&appid=".$myToken);
+        $response5 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city5."&units=metric&appid=".$myToken);
+        $response6 = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city6."&units=metric&appid=".$myToken);
         
         $response_array = [$response1,$response2,$response3,$response4,$response5,$response6];
         
@@ -29,8 +30,10 @@ class ProjectController extends Controller
 
     public function search(Request $request)
     {
+
+            $myToken = "82e0be784317765ec3af35fc2e2d4aef";
             $city =  $request['cityName'];
-            $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city."&units=metric&appid=82e0be784317765ec3af35fc2e2d4aef");
+            $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q=".$city."&units=metric&appid=".$myToken);
             //dd($response);
             return view('search', ['response'=>$response]);
 
